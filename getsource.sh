b8330f919e8b9549d80cae8a1efc9103e1b5761d
Colin Smith <colin.smith@corfinancialgroup.com>
2018-09-05 11:39:47 +0100

    wrong place

--- getsource.sh
+++ getsource.sh
@@ -7,11 +7,11 @@ done
 cp $BASE/validate.h libsafeqp
 cp $BASE/dsmxainv.c libsafeqp
 mkdir m4 config
+mkdir RPM/SOURCES
+mkdir RPM/SOURCES/libsafeqp-1
 cd libsafeqp
 rm constant.*
 genconst -x > constant.cpp
 genconst -h > constant.h
-mkdir RPM/SOURCES
-mkdir RPM/SOURCES/libsafeqp-1
 #Investigate this
 #find . -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | xargs md5sum > DEBIAN/md5sums
